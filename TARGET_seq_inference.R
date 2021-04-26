5ec71ed854c093577141fb428a06ad5d797e38cd
Christophe Desterke <cdesterke@users.noreply.github.com>
2021-04-26 11:39:07 +0200

    Update TARGET_seq_inference.R

--- TARGET_seq_inference.R
+++ TARGET_seq_inference.R
@@ -1,8 +1,13 @@
 
+## load single cell phenotype from seurat object
+
+data<-read.csv("exportpheno.csv",sep="\t",row.names=1)
+
+
 library(ggplot2)
 
 #####  
-df <- data.frame(x = data$mutations, z = data$clusters)
+df <- data.frame(x = data$mutations, z = data$seurat_clusters)
 df <- as.data.frame(with(df, prop.table(table(x, z), margin = NULL)))
 
 
@@ -13,7 +18,7 @@ plot <- ggplot(data = df, aes(x = x, y = Freq, fill = z)) +
   scale_y_continuous(expand = c(0.01, 0), labels = scales::percent_format()) + 
   xlab("mutations") + 
   ylab("Percent") + 
-  labs(fill = "treatment") + 
+  labs(fill = "seurat_clusters") + 
   theme_classic(base_size = 12, base_family = "sans") + 
   theme(legend.position = "right") + coord_flip()
 plot
